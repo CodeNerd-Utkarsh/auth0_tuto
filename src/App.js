624a5acc5fc12e1473bd8ea6afaea3312b43c080
CondeNerd-Utkarsh <hbase_groups_official@aol.com>
2022-02-26 12:27:19 +0530

    login button created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Login from './components/Login'
+import Logout from './components/Logout'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Login />
+            {/* <Logout /> */}
+        </div>
     )
 }
 
---
+++ src/components/Logout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+const Logout = () => {
+
+    return (
+        <button
+        // onClick={(e)=>}
+        >Logout</button>
+    )
+}
+
+export default Logout
