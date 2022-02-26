12027e5caa973af8ff9cdd7d2439d19623a6c4f0
CondeNerd-Utkarsh <hbase_groups_official@aol.com>
2022-02-26 15:51:48 +0530

    let's check out if we are logged In or not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './components/Login'
 import Logout from './components/Logout'
+import UserProfile from './components/UserProfile'
 
 const App = () => {
     return (
         <div>
             <Login />
             <Logout />
+            <UserProfile />
         </div>
     )
 }
