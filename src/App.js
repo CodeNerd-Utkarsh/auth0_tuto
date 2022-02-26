ab291f20e7e68503d2c947fa3a264f36f69b1dd7
CondeNerd-Utkarsh <hbase_groups_official@aol.com>
2022-02-26 15:31:11 +0530

    logout button created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     return (
         <div>
             <Login />
-            {/* <Logout /> */}
+            <Logout />
         </div>
     )
 }
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 const Logout = () => {
-
+    const { logout } = useAuth0();
     return (
         <button
-        // onClick={(e)=>}
+            onClick={(e) => logout()}
         >Logout</button>
     )
 }
